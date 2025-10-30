a0ca5a4e86d880c1c8ff29854399046954a37b32
Gowri855 <gowriM@08052005@gmail.com>
2025-10-30 22:33:43 +0530

    finalised

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
-let sections=document.querySelectorAll('section');
-let navlinks=document.querySelectorAll('header nav a');
+/* ---------------- Menu and Navbar Toggle Section Start ---------------- */
 
-window.onscroll=()=>{
+// Selecting elements
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+/* ---------------- Active Navigation on Scroll Section Start ---------------- */
+
+// Selecting sections and navigation links
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+// Scroll event for updating active link based on current section
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height){
+
+        // Highlight active link when section is in view
+        if (top >= offset && top < offset + height) {
             navlinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ' ]').classList.add ('active')
-            })
+                document
+                    .querySelector('header nav a[href*=' + id + ' ]')
+                    .classList.add('active');
+            });
         }
-    })
+    });
+};
 
-}
-menuIcon.onclick=()=>{
+/* ---------------- Active Navigation on Scroll Section End ---------------- */
+
+/* ---------------- Menu Icon Click Event Section Start ---------------- */
+
+// Toggle menu icon and navbar visibility on click
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('active');   
-}
+    navbar.classList.toggle('active');
+};
+
+/* ---------------- Menu Icon Click Event Section End ---------------- */
+
+/* ---------------- Menu and Navbar Toggle Section End ---------------- */
+
